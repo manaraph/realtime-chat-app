fdc25269687bed5404aeb683880b1e856be27743
manaraph <manaraph225@gmail.com>
2023-02-14 01:42:00 +0100

    Add models

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
